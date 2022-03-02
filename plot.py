267c047b6350033df2f09198e25007c86d688eb2
Jamal El Youssefi <jamal.elyoussefi@yahoo.fr>
2022-03-02 20:57:37 +0100

    Update

--- plot.py
+++ plot.py
@@ -9,7 +9,8 @@ f = open('./output/highPrio.json')
 configHP = json.load(f)
 f.close()
 
-totalMemGb = float(configLP["totalMemSize"])/(1024*1024*1024)
+totalMemSizeGb = float(configLP["totalMemSize"])/(1024*1024*1024)
+lpMemSizeGb = totalMemSizeGb/float(configLP['memSizeRatio'])
 
 xLP, yLP, xHP, yHP = [], [], [], []
  
@@ -41,6 +42,8 @@ for i in range(0, len(xLP)-1)  :
 		splitIdx = i;
 		break;
 		
+x1LP, y1LP, x2LP, y2LP = [], [], [], []
+
 if splitIdx != -1:
 	x1LP = xLP[:splitIdx]
 	y1LP = yLP[:splitIdx]
@@ -62,24 +65,39 @@ yHPLimit = [ yHP[0], yHP[-1] ]
 	
 x1LP[:] = [float(x - x0)/1000 for x in x1LP]
 xHP[:] = [float(x - x0)/1000 for x in xHP]
-if splitIdx != -1:
+xResume, yResume = [], [] 
+if len(x2LP) > 0:
 	x2LP[:] = [float(x - x0)/1000 for x in x2LP]
+	xResume.append(x2LP[0]);
+	yResume.append(lpMemSizeGb/2);
+	for i in range(len(y2LP)):
+		if y2LP[i] >= lpMemSizeGb:
+			xResume.append(x2LP[i]);
+			yResume.append(lpMemSizeGb/2);
+			break;
+	
 xLPLimit[:] = [float(x - x0)/1000 for x in xLPLimit]
 xHPLimit[:] = [float(x - x0)/1000 for x in xHPLimit]
 
+lw=2.5
 
-plt.plot(x1LP, y1LP, color='g', label='Low Priority Process ({}%)'.format(int(configLP['memSizeRatio']*100)))
-if splitIdx != -1:
-	plt.plot(x2LP, y2LP, color='g')
-plt.plot(xHP, yHP, color='r', label='High Priority Process ({}%)'.format(int(configHP['memSizeRatio']*100)))
+plt.plot(x1LP, y1LP, lw=lw, color='g', label='Low Priority Process ({}%)'.format(int(configLP['memSizeRatio']*100)))
+if len(x2LP) > 0:
+	plt.plot(x2LP, y2LP, lw=lw, color='g')
+plt.plot(xHP, yHP, lw=lw, color='r', label='High Priority Process ({}%)'.format(int(configHP['memSizeRatio']*100)))
 plt.scatter(xLPLimit, yLPLimit, color='g')
 plt.scatter(xHPLimit, yHPLimit, color='r')
-plt.hlines(totalMemGb, xLPLimit[0], xLPLimit[-1], color='b', linestyles='dashed', label='Total Memory size (GB)')
+plt.hlines(totalMemSizeGb, xLPLimit[0], xLPLimit[-1], color='b', linestyles='dashed', label='Total Memory size (GB)')
+if len(xResume) > 0:
+	plt.vlines(xResume[0], 0, totalMemSizeGb, lw=1, color='k', linestyles='dashed')
+	plt.vlines(xResume[1], 0, totalMemSizeGb, lw=1, color='k', linestyles='dashed')
+	plt.plot(xResume, yResume, color='c', marker='o', label='Resume time {} ms'.format(format(xResume[1]-xResume[0], '.1f')))
+
 plt.legend(loc="lower right")
 plt.xlabel('Timestamp (s)')
 plt.ylabel('Allocated memory size (GB)')
 plt.xlim(xmin=0)
-plt.ylim(ymin=0)
+#plt.ylim(ymin=0)
 plt.title("Memory Eviction");
 plt.show()
 
