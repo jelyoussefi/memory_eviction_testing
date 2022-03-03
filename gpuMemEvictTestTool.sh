1269ec054f18ba91cfcf5f5748aa2a50ca4f806f
Jamal El Youssefi <jamal.elyoussefi@yahoo.fr>
2022-03-03 22:52:00 +0100

    update

--- gpuMemEvictTestTool.sh
+++ gpuMemEvictTestTool.sh
@@ -7,11 +7,13 @@ trap "kill ${lwpPid[@]}" SIGINT
 
 sleep 15
 
+echo  -e "$(tput setaf 3)\n\tSuspending Process $(tput sgr 0)"$lwpPid ;
 kill -STOP $lwpPid
 
 ./gpuMemEvictTestTool -m $2 -t 20 -h
 
-kill -STOP $lwpPid
+echo  -e "$(tput setaf 3)\n\tResuming Process $(tput sgr 0)"$lwpPid ;
+kill -CONT $lwpPid
 
 wait $lwpPid
 
